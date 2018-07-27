0808d2e67aaf2a279cd42dc4fb857508e0ee12ca
Kay B <kaylouisebennett@gmail.com>
2018-07-27 16:37:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,19 @@
 var playlist = { artist: "song title" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
   playlist[artistName] = "song title";
+=======
+  playlist[artist] = "song title";
+>>>>>>> 9576fb570aadfdc88093a5c6a388cfb8253d338c
   return playlist;
 } 
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
 delete playlist[artistName];
+=======
+ delete playlist.artistName;
+>>>>>>> 9576fb570aadfdc88093a5c6a388cfb8253d338c
 return playlist;
 }
